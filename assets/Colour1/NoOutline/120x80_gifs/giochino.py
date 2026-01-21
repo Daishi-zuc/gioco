4564b7a5389908c9506264424e447480669a0750
Daishi-zuc <angelo.cheng@itiszuccante.edu.it>
2026-01-21 11:39:29 +0100

    sfondo

---
+++ assets/Colour1/NoOutline/120x80_gifs/giochino.py
@@ -0,0 +1,31 @@
+import arcade
+import random
+
+class giochino(arcade.Window):
+    def __init__(self, larghezza, altezza, titolo):
+        super().__init__(larghezza, altezza, titolo)
+
+        self.background = None
+        self.lista_background = arcade.SpriteList()
+
+    def setup(self):
+
+        self.wallpaper()
+
+    def wallpaper(self):
+        self.background=arcade.Sprite("./assets/background.png")
+        self.background.center_x = 600
+        self.background.center_y = 600
+        self.background.scale = 4
+        self.lista_background.append(self.background)
+
+    def on_draw(self):
+        self.clear()
+        self.lista_background.draw()
+
+def main():    
+    gioco = giochino(600, 600, "Giochino")    
+    arcade.run()
+
+if __name__ == '__main__':
+    main()
