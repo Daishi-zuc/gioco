ea77f7e790ec95e8450a4457302da300874dcf37
Daishi-zuc <angelo.cheng@itiszuccante.edu.it>
2026-01-21 11:50:07 +0100

    p

--- assets/Colour1/NoOutline/120x80_gifs/giochino.py
+++ assets/Colour1/NoOutline/120x80_gifs/giochino.py
@@ -5,13 +5,21 @@ class giochino(arcade.Window):
     def __init__(self, larghezza, altezza, titolo):
         super().__init__(larghezza, altezza, titolo)
 
+        self.character = None
         self.background = None
+        self.lista_character = arcade.Spritelist()
         self.lista_background = arcade.SpriteList()
         self.setup()
 
     def setup(self):
 
         self.wallpaper()
+        
+        self.character = arcade.Sprite("./assets/Colour1/NoOutline/120x80_gifs/__Idle.gif")
+        self.character.center_x = 300
+        self.character.center_y = 100
+        self.character.scale = 1.0
+        self.lista_character.append(self.character)
 
     def wallpaper(self):
         self.background=arcade.Sprite("./assets/background.png")
@@ -23,6 +31,7 @@ class giochino(arcade.Window):
     def on_draw(self):
         self.clear()
         self.lista_background.draw()
+        self.lista_character.draw()
 
 def main():    
     gioco = giochino(600, 600, "Giochino")    
